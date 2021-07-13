26aaa00c659382ed1d70d5960f2c1e678342c3a3
unknown <tombrzoza50@gmail.com>
2021-07-13 23:00:46 +0200

    modified NavigationPage

--- src/Layout/Navigation.js
+++ src/Layout/Navigation.js
@@ -5,7 +5,7 @@ import '../Styles/Navigation.css';
 const Navigation = () => {
   return (
     <ul className='main__navigation_ul'>
-      <li className='main__navigation_li-item'><NavLink className=' nav__link nav__link1' to="/"></NavLink></li>
+      <li className='main__navigation_li-item'><NavLink className='nav__link nav__link1' to="/"></NavLink></li>
       <li className='main__navigation_li-item'><NavLink className='nav__link nav__link2' to="about"></NavLink></li>
       <li className='main__navigation_li-item'><NavLink className='nav__link nav__link3' to="work"></NavLink></li>
       <li className='main__navigation_li-item'><NavLink className='nav__link nav__link4' to="contact"></NavLink></li>
